d248b7498ef5429d250017f64eebee4f84e14f9d
AliciaRus <116314121+AliciaRus@users.noreply.github.com>
2023-02-13 17:14:33 +0000

    test

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from pygame.locals import QUIT
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('Hello Meg!')
+clock = pygame.time.Clock() 
+oog = input("whatever you want meg")
+print(oog)
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+    clock.tick(30)
