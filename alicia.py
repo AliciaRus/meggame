b6ec12917f6032df633b00831fc92113ef0005af
AliciaRus <116314121+AliciaRus@users.noreply.github.com>
2023-02-13 17:32:50 +0000

    megio

--- alicia.py
+++ alicia.py
@@ -5,13 +5,16 @@ pygame.init()
 screen = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello Meg!')
 clock = pygame.time.Clock() 
-oog = input("whatever you want meg")
-print(oog)
+
+megio = pygame.image.load('megio.png')
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    
+    pygame.draw.circle(screen, (255,0,255), (200, 200), 50, 1)
+    screen.blit(megio,(50,50))
     pygame.display.update()
     clock.tick(30)
-    pygame.draw.circle(screen, (0,0,255), (150, 50), 15, 1)
